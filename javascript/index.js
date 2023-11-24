34f600f20229e262d352758656ead5b471d1ce21
Hazel <Hazelgreasley@hotmail.com>
2023-11-24 14:03:58 +0800

    Minor tweaks

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+// Los Angeles
+
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+losAngelesDateElement.innerHTML = "U SUCK";
+losAngelesTimeElement.innerHTML = "1:48:15<small>AM</small>";
